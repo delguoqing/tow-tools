31ef5a1849bedb4834aa3baba7799116d5ab5066
GQ <delguoqing@hotmail.com>
2013-11-09 00:49:48 +0800

    socket flag and offset look up

--- src/ani_parser/ani_parser.py
+++ src/ani_parser/ani_parser.py
@@ -1,9 +1,11 @@
 import sys
 import operator
 import struct
+import numpy
 
 MAGIC_CODE = "ANI\x00"
 
+BIT_USE_MATRIX = 0x100	# use matrix directly
 BIT_HAS_TRANSLATE = 0x1
 BIT_HAS_ROTATE = 0x2	# in the form of quaternion.
 						# This can be verfied easily by calculate the norm.
@@ -51,28 +53,36 @@ def parse(data):
 		print "%s  @offset:0x%x" % (bone_name, ani_offset)
 		
 		# guess
-		key_count, unknown = _G(ani_offset, "<II")
-		print "key_frame_count = %d" % (key_count, )
+		key_count, flag_all = _G(ani_offset, "<II")
+		print "key_frame_count = %d, flag_all = 0x%x" % (key_count, flag_all)
 		
-		if unknown != 0x7:
-			print "==============>unknown = 0x%x" % unknown
-		#assert unknown == 7, str(unknown)
-		print "key frames @: ", ",".join(map(repr, _G(ani_offset + 0x8, "<" + "I" * key_count)))
+		key_frame_offsets = _G(ani_offset + 0x8, "<" + "I" * key_count)
+
+		key_frame_offset_next = ani_offset + 0x8 + key_count * 0x4
 		
-		unknown2, = _G(ani_offset + 0x8 + 0x4 * key_count, "<I")
-		unknown2_half = (unknown2 >> 16)
-		assert unknown2_half == unknown, ("what?0x%x" % unknown2_half)
-		#assert unknown2 == 0x70000, str(unknown2)
-		if unknown2 != 0x70000:
-			print "==============>unknown2 = 0x%x" % unknown2
-		
-		key_frame_offset = ani_offset + 0x8 + 0x4 * key_count
 		for key_index in xrange(key_count):
+			key_frame_offset = key_frame_offsets[key_index] + ani_offset
+			
+			assert key_frame_offset_next == key_frame_offset, "not matched!"
+			
 			flag1, flag2 = _G(key_frame_offset, "<HH")
 			key_frame_offset += 4
 			print "key@frame %4d, flags=0x%x" % (flag1, flag2)
 			
+			use_matrix = (flag2 & BIT_USE_MATRIX)
+			if use_matrix:
+				jii = _G(key_frame_offset, "<" + "f" * 9)
+				arr = numpy.array(jii)
+				arr.shape = (3, 3)
+				print "socket:"
+				print numpy.mat(arr)
+				
+				sys.stderr.write("socket @ %s\n" % bone_name)
+				
+				key_frame_offset += 0x9 * 0x4
+			
 			float_count = 0
+				
 			if flag2 & BIT_HAS_TRANSLATE:
 				float_count += 3
 			if flag2 & BIT_HAS_ROTATE:
@@ -96,7 +106,7 @@ def parse(data):
 				print "\tscale: (%f, %f, %f)" % floats[:3]
 				floats = floats[3:]
 				
-			key_frame_offset += 0x4 * float_count
+			key_frame_offset_next = key_frame_offset + 0x4 * float_count
 		
 		print
 		
